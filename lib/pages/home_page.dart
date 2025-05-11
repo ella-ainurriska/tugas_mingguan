651064be0f06ad0135411e41f6706b5473b3ab87
icha <ellaainurriska1@gmail.com>
2025-05-11 22:54:45 +0700

    Tambahkan input field dan tombol tambah untuk tugas baru

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -54,6 +54,39 @@ class _HomePageState extends State<HomePage> {
           );
         },
       ),
+         floatingActionButton: Row (
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'Tulis Tugas Baru',
+                  filled: true,
+                  fillColor: Colors.deepPurple.shade200,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.deepPurple,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                 focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        FloatingActionButton(
+        onPressed: saveNewTask,
+        child: const Icon(Icons.add),
+        ),
+       ],
+      ),
     );
   }
 }
