b2e5d52b32800fbe526ebc14ad7eea25d02f0876
icha <ellaainurriska1@gmail.com>
2025-05-11 22:19:23 +0700

    menambahkan beberapa fungsi

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,27 @@ class _HomePageState extends State<HomePage> {
   final _controller = TextEditingController();
   List toDolist = [];
 
-  
+
+  void checkBoxChanged(int index) {
+    setState(() {
+      toDolist[index][1] = !toDolist[index][1];
+    });
+  }
+
+  void saveNewTask(){
+    setState(() {
+      toDolist.add([_controller.text, false]);
+      _controller.clear();
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      toDolist.removeAt(index);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
