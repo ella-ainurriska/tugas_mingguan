02bd7dd7b70903729f02174e77b2be7b413b721d
icha <ellaainurriska1@gmail.com>
2025-05-12 19:49:37 +0700

    Buat struktur awal TodoList dan style

---
+++ lib/utils/todo_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoList extends StatefulWidget {
+  const TodoList({
+    super.key
+    required this.tasName,
+    required this.taskCompleted,
+    required this.onChanged,
+    required this.deleteFunction,
+    });
+
+  final String tasName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 0),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
